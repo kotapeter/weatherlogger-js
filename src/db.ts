cd89ae048c355c6b9f3c75f007f255cca8be721b
Daniel Pataki <daniel@kinsta.com>
2022-05-23 13:41:42 +0200

    Removing db table name from env variables

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,14 @@
 import dbs from './dbs'
 const db = dbs[process.env.DB_TYPE]
+const dbTableName = 'weatherdata'
 
 const dbFunctions = {
   knex: {
-    insertWeatherData: async weatherData => db.connection(process.env.DB_TABLE).insert(weatherData),
-    listWeatherData: async () => db.connection(process.env.DB_TABLE).select(['time', 'temp', 'wind']).orderBy('time', 'desc').limit(100),
+    insertWeatherData: async weatherData => db.connection(dbTableName).insert(weatherData),
+    listWeatherData: async () => db.connection(dbTableName).select(['time', 'temp', 'wind']).orderBy('time', 'desc').limit(100),
     setup: async () => {
-      if(!await db.connection.schema.hasTable(process.env.DB_TABLE)) {
-        await db.connection.schema.createTable(process.env.DB_TABLE, (table) => {
+      if(!await db.connection.schema.hasTable(dbTableName)) {
+        await db.connection.schema.createTable(dbTableName, (table) => {
           table.increments('id').primary();
           table.string('time', 22);
           table.float('temp', 2);
@@ -19,21 +20,21 @@ const dbFunctions = {
   mongodb: {
     insertWeatherData: async weatherData => {
       const mongo = await db.connection()
-      return mongo.collection(process.env.DB_TABLE).insertOne({...weatherData})
+      return mongo.collection(dbTableName).insertOne({...weatherData})
     },
     listWeatherData: async () => {
       const mongo = await db.connection()
-      return mongo.collection(process.env.DB_TABLE).find({}, {projection: { _id: 0 }}).sort({ time: -1 }).limit(100).toArray()
+      return mongo.collection(dbTableName).find({}, {projection: { _id: 0 }}).sort({ time: -1 }).limit(100).toArray()
     }
   },
   redis: {
     insertWeatherData: async weatherData => {
       const redis = await db.connection()
-      return redis.set(process.env.DB_TABLE, JSON.stringify(weatherData))
+      return redis.set(dbTableName, JSON.stringify(weatherData))
     },
     listWeatherData: async () => {
       const redis = await db.connection()
-      return redis.get(process.env.DB_TABLE).then(result => JSON.parse(result))
+      return redis.get(dbTableName).then(result => JSON.parse(result))
     }
   }
 }
